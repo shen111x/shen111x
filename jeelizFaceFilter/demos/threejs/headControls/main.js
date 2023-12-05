27624b48dc43a750cb3817ecdc2ed4635bb7f6b0
shen111x <150092349+shen111x@users.noreply.github.com>
2023-12-05 04:39:20 -0800

    1

--- jeelizFaceFilter/demos/threejs/headControls/main.js
+++ jeelizFaceFilter/demos/threejs/headControls/main.js
@@ -4,7 +4,7 @@ let camera = null, controls = null, scene = null, renderer = null;
 function main() {
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0xffffff );
+  scene.background = new THREE.color( 0xffffff );
   scene.fog = new THREE.FogExp2( 0xcccccc, 0.2 );
 
   renderer = new THREE.WebGLRenderer();
@@ -35,26 +35,16 @@ function main() {
 
 
 
-  --------  --------  --------  --------  --------  --------  --------  --------  --------  --------  --------  --------
-  //for ( let i = 0; i < 500; ++ i ) {
-//
-  //  const mesh = new THREE.Mesh( geometry, material );
-    //mesh.position.x = ( Math.random() - 0.5 ) * 1000;
-    //mesh.position.y = ( Math.random() - 0.5 ) * 1000;
-    //mesh.position.z = ( Math.random() - 0.5 ) * 1000;
-    //mesh.updateMatrix();
-    //mesh.matrixAutoUpdate = false;
-    //scene.add( mesh );
+  for ( let i = 0; i < 500; ++ i ) {
 
-     const loader = new GLTFLoader();
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.position.x = ( Math.random() - 0.5 ) * 1000;
+    mesh.position.y = ( Math.random() - 0.5 ) * 1000;
+    mesh.position.z = ( Math.random() - 0.5 ) * 1000;
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    scene.add( mesh );
 
-        // 加载GLTF模型
-        loader.load('3d/1.gltf', (gltf) => {
-            // 将模型添加到场景
-            scene.add(gltf.scene);
-        });
-  }
- --------  --------  --------  --------  --------  --------  --------  --------  --------  --------  --------  --------
   // lights:
   const light0 = new THREE.DirectionalLight( 0xffffff );
   light0.position.set( 1, 1, 1 );
