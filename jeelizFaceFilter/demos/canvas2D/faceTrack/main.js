b20de08acd4a1b097da87a6a4bf0a2bf9d44fba9
shen111x <150092349+shen111x@users.noreply.github.com>
2023-12-05 11:37:29 -0800

    1

--- jeelizFaceFilter/demos/canvas2D/faceTrack/main.js
+++ jeelizFaceFilter/demos/canvas2D/faceTrack/main.js
@@ -12,7 +12,7 @@ function main(){
 
       console.log('INFO: JEELIZFACEFILTER IS READY');
       CVD = JeelizCanvas2DHelper(spec);
-      CVD.ctx.lineWidth = 50;
+      CVD.ctx.lineWidth = 80;
       CVD.ctx.strokeStyle = "#000000";
       CVD.ctx.fillStyle = "#000000";
       
@@ -20,50 +20,13 @@ function main(){
     },
 
     // called at each render iteration (drawing loop):
-    callbackTrack: function(detectState){ 
-        if (detectState.detected > 0.8){
-          const faceCoo = CVD.getCoordinates(detectState);
-          CVD.ctx.clearRect(0, 0, CVD.canvas.width, CVD.canvas.height);
-
-          const trailElement = document.createElement("div");
-          trailElement.className = "trail";
-
-
-          var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-          // 计算新的 left 值
-          var newLeftValue = (600 / faceCoo.x) * screenWidth;
-
-          var screenheight = window.innerHeight || document.documentElement.clientheight || document.body.clientheight;
-
-          // 计算新的 left 值
-          var newLeftValue2 = (600 / faceCoo.x) * screenheight;
-
-          trailElement.style.left = newLeftValue + "px";
-          trailElement.style.top = newLeftValue2 + "px";
-          
-          // 设置 trailElement 的 left 值
-          //trailElement.style.left = newLeftValue + "px";
-          //trailElement.style.left = faceCoo.x + "px";
-          trailElement.style.top = faceCoo.y + "px";
-
-
-
-
-
-          document.body.appendChild(trailElement);
-
-           // 移除多余的轨迹元素
-          const trailElements = document.querySelectorAll(".trail");
-          if (trailElements.length > 100) {
-          document.body.removeChild(trailElements[0]);
-          }
-
+    callbackTrack: function(detectState){
+      if (detectState.detected > 0.8){
         // draw a border around the face:
-        //
-        //CVD.ctx.clearRect(0, 0, CVD.canvas.width, CVD.canvas.height);
-        //CVD.ctx.strokeRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
-        //CVD.ctx.fillRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
+        const faceCoo = CVD.getCoordinates(detectState);
+        CVD.ctx.clearRect(0, 0, CVD.canvas.width, CVD.canvas.height);
+        CVD.ctx.strokeRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
+        CVD.ctx.fillRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
         CVD.update_canvasTexture();
       }
       CVD.draw();
